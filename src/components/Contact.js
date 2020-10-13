99a07764067d357670fccd217d8f9ae54ada1f2f
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-13 10:28:33 +0200

    added props to contact.js + rendered 3 contacts in App + added proptypes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './Contact.css';
 
-const avatar =
-  'https://img.buzzfeed.com/buzzfeed-static/static/2015-01/28/15/campaign_images/webdr11/if-silent-bob-quotes-were-motivational-posters-2-7063-1422478686-11_big.jpg';
-const name = 'Silent Bob';
-const online = true;
-
-const Contact = () => {
+const Contact = (props) => {
   return (
     <div className='Contact'>
-      <img src={avatar} className='avatar'></img>
+      <img src={props.avatar} alt={props.name} className='avatar'></img>
       <div>
-        <h4 className='name'>{name}</h4>
+        <h4 className='name'>{props.name}</h4>
         <div className='status'>
-          <span className={online ? 'status-online' : 'status-offline'}></span>
-          <p className='status-text'>{online ? 'online' : 'offline'}</p>
+          <span
+            className={
+              props.online ? 'status-online' : 'status-offline'
+            }></span>
+          <p className='status-text'>{props.online ? 'online' : 'offline'}</p>
         </div>
       </div>
     </div>
