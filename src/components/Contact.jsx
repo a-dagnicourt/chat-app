c0c987745a8ee26cd3ae4278b89b2b0f44f4821a
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-14 10:33:08 +0200

    added status change feature via React state switching Contact from functional to class component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import './Contact.css';
 
-const Contact = (props) => {
-  return (
-    <div className='Contact'>
-      <img src={props.avatar} alt={props.name} className='avatar'></img>
-      <div>
-        <h4 className='name'>{props.name}</h4>
-        <div className='status'>
-          <span
-            className={
-              props.online ? 'status-online' : 'status-offline'
-            }></span>
-          <p className='status-text'>{props.online ? 'online' : 'offline'}</p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online,
+    };
+  }
+
+  render() {
+    return (
+      <div className='Contact'>
+        <img
+          src={this.props.avatar}
+          alt={this.props.name}
+          className='avatar'></img>
+        <div>
+          <h4 className='name'>{this.props.name}</h4>
+          <div
+            className='status'
+            onClick={(e) => {
+              const newStatus = !this.state.online;
+              this.setState({
+                online: newStatus,
+              });
+            }}>
+            <span
+              className={
+                this.state.online ? 'status-online' : 'status-offline'
+              }></span>
+            <p className='status-text'>
+              {this.state.online ? 'online' : 'offline'}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Contact;
