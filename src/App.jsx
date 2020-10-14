3d8b06f5be5fdce0903d2a8cf091f93b7e10c539
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-14 10:22:17 +0200

    Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ContactList from './components/ContactList';
 function App() {
   return (
     <div className='App'>
-      <ContactList/>
+      <ContactList />
     </div>
   );
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,39 +2,44 @@ import React from 'react';
 import Contact from './Contact';
 
 const users = [
-    {
-      name: 'Colonel Reyes',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      online: false
-    },
-    {
-      name: 'Nellie Caldwellwellwell',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true
-    },
-    {
-      name: 'Vernon Freemason',
-      avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
-    },
-    {
-      name: 'Erica Hunterixhunter',
-      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
-    },
-    {
-      name: 'Juanita Electrolux',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
-    }
-  ];
+  {
+    name: 'Colonel Reyes',
+    avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
+    online: false,
+  },
+  {
+    name: 'Nellie Caldwellwellwell',
+    avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
+    online: true,
+  },
+  {
+    name: 'Vernon Freemason',
+    avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
+    online: true,
+  },
+  {
+    name: 'Erica Hunterixhunter',
+    avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
+    online: false,
+  },
+  {
+    name: 'Juanita Electrolux',
+    avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
+    online: true,
+  },
+];
 
-  const ContactList = () => (
-      <div>
-          {users.map(user => (
-          <Contact name={user.name} avatar={user.avatar} online={user.online} key={user.name}/>
-          ))}
-      </div>
-  )
+const ContactList = () => (
+  <div>
+    {users.map((user) => (
+      <Contact
+        name={user.name}
+        avatar={user.avatar}
+        online={user.online}
+        key={user.name}
+      />
+    ))}
+  </div>
+);
 
-  export default ContactList;
+export default ContactList;
